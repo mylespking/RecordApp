a2362dc6b059e4f57c71038f14f3f5a13a8ae42a
mylespking <64848102+mylespking@users.noreply.github.com>
2022-10-19 09:17:16 +0100

    Created page for seeing record collection

---
+++ RecordApp/Controllers/CollectionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RecordApp.Models;
+
+namespace RecordApp.Controllers
+{
+    public class CollectionController : Controller
+    {
+        private readonly ILogger<CollectionController> _logger;
+
+        public CollectionController(ILogger<CollectionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
